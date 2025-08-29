39ece6733a0a595c784d43341f5f6f96f67d9cee
li-jiang-yan <professional.jiangyan_li@outlook.com>
2025-08-29 18:52:25 +0300

    Added linear regressor for simple linear regression

--- simple.py
+++ simple.py
@@ -1,5 +1,6 @@
 from sklearn.datasets import load_diabetes
 from sklearn.model_selection import train_test_split
+from sklearn import linear_model
 
 # Load diabetes dataset
 diabetes = load_diabetes()
@@ -8,3 +9,9 @@ y = diabetes.target
 
 # Split data points for training and testing
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
+
+# Create linear regressor
+reg = linear_model.LinearRegression()
+reg.fit(X_train, y_train)
+m = reg.coef_
+c = reg.intercept_
