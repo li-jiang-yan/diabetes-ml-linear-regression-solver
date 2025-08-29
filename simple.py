467de09c4ce336b035b31a455d8e749dd8376f33
li-jiang-yan <professional.jiangyan_li@outlook.com>
2025-08-29 18:46:05 +0300

    Added splitting of training and testing data for simple linear regression

--- simple.py
+++ simple.py
@@ -1,6 +1,10 @@
 from sklearn.datasets import load_diabetes
+from sklearn.model_selection import train_test_split
 
 # Load diabetes dataset
 diabetes = load_diabetes()
 X = diabetes.data
 y = diabetes.target
+
+# Split data points for training and testing
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
